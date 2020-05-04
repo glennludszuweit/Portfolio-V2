bf8785e3aa6ecf59165a0b403ccc2ec0c2cfe9b1
glenn <glenn.ludszuweit@gmail.com>
2020-05-04 11:57:58 +0200

    installed and setup ejs

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("views/index");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
