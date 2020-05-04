2fec41c436ad14f300c5536397f17e2ed66cc68f
glenn <glenn.ludszuweit@gmail.com>
2020-05-04 12:15:30 +0200

    setup static directory

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 9000;
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("views/index");
+  res.render("index");
 });
 
 app.listen(port, () => {
