aeaa833f8a38150bb37dfd6a3aae4ec2a19d40ad
glenn <glenn.ludszuweit@gmail.com>
2020-05-04 14:01:43 +0200

    use handlebars instead of ejs

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const path = require("path");
 const nodemailer = require("nodemailer");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.set("view engine", "ejs");
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use(
   express.urlencoded({
     extended: false,
@@ -34,17 +38,17 @@ app.post("/send", (req, res) => {
       `;
 
   let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.mailtrap.io",
     port: 587,
     secure: false,
     auth: {
-      user: "gnglab.dev@gmail.com", // generated ethereal user
-      pass: "********", // generated ethereal password
+      user: "d262be34a8992c", // generated ethereal user
+      pass: "cc5cf3e73fc3cb", // generated ethereal password
     },
   });
 
   let mailOptions = {
-    from: '"glennludszuweit.de" <gnglab.dev@gmail.com>', // sender address
+    from: '"glennludszuweit.de" <d262be34a8992c>', // sender address
     to: "glenn.ludszuweit@gmail.com", // list of receivers
     subject: `Profile Message from ${req.body.email}`, // Subject line
     text: "", // plain text body
@@ -58,7 +62,7 @@ app.post("/send", (req, res) => {
     console.log("Message sent: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-    res.render("/");
+    res.redirect("/#contact", { msg: "Email Sent!" });
   });
 });
 
